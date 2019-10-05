3f0a51e3017c53429b988d94637595bcf83a8d1e
manojchaudhary04 <manoj.chaudhary_cs18@gla.ac.in>
2019-10-05 22:24:17 +0530

    Initial Commit

---
+++ src/stringhandling/Solution3.java
@@ -0,0 +1,18 @@
+package stringhandling;
+
+import java.util.Scanner;
+
+public class Solution3 {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        s = s.replace('a', '*');
+        s = s.replace('e', '*');
+        s = s.replace('i', '*');
+        s = s.replace('0', '*');
+        s = s.replace('u', '*');
+        System.out.println(s);
+    }
+}
+
